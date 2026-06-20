c2cb1991db9fe10538a28bff756dad1c5fb8efaf
chirrus <47577505+chirrus@users.noreply.github.com>
2026-06-20 11:28:59 -0300

    Update plugin version and release number

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'mod_doc2interact';
-$plugin->version      = 2026060737;
+$plugin->version      = 2026061002;
 $plugin->requires     = 2022041900; // Moodle 4.0+
 $plugin->maturity     = MATURITY_STABLE;
-$plugin->release      = '1.0.0';
+$plugin->release      = '1.1.0';
