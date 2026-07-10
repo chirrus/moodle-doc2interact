431619f5e506531dae105975012cc169e453237b
chirrus <47577505+chirrus@users.noreply.github.com>
2026-07-10 10:08:14 -0300

    Implement doc2interact_view function
    Added a function to mark the activity as viewed and trigger the course_module_viewed event.

--- lib.php
+++ lib.php
@@ -1,4 +1,19 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle. If not, see <http://www.gnu.org/licenses/>.
+
 defined('MOODLE_INTERNAL') || die();
 
 function doc2interact_add_instance($data) {
@@ -29,7 +44,7 @@ function doc2interact_supports($feature) {
     }
 }
 
-// Servir archivos HTML generados (contenido + autoevaluación)
+// Serve generated HTML files (content + self-assessment)
 function doc2interact_pluginfile($course, $cm, $context, $filearea, $args, $forcedownload, array $options = []) {
     if ($context->contextlevel != CONTEXT_MODULE) return false;
     require_login($course, true, $cm);
@@ -45,4 +60,27 @@ function doc2interact_pluginfile($course, $cm, $context, $filearea, $args, $forc
 
     // Servir como HTML sin forzar descarga
     send_stored_file($file, 0, 0, false, $options);
+}
+
+/**
+ * Mark the activity viewed and trigger the course_module_viewed event.
+ *
+ * @param stdClass $doc2interact doc2interact instance
+ * @param stdClass $course Course object
+ * @param stdClass $cm Course module object
+ * @param context_module $context Context module object
+ */
+function doc2interact_view(stdClass $doc2interact, stdClass $course, stdClass $cm, context_module $context): void {
+    // Trigger module viewed event.
+    $event = \mod_doc2interact\event\course_module_viewed::create([
+        'objectid' => $doc2interact->id,
+        'context'  => $context,
+    ]);
+    $event->add_record_snapshot('course', $course);
+    $event->add_record_snapshot('doc2interact', $doc2interact);
+    $event->trigger();
+
+    // Completion.
+    $completion = new \completion_info($course);
+    $completion->set_module_viewed($cm);
 }
