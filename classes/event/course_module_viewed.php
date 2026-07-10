d8fc31590f31e094aef0ffdeb32d8e2fe775df78
chirrus <47577505+chirrus@users.noreply.github.com>
2026-07-10 10:05:57 -0300

    Add course_module_viewed event for doc2interact

---
+++ classes/event/course_module_viewed.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle. If not, see <http://www.gnu.org/licenses/>.
+
+namespace mod_doc2interact\event;
+
+/**
+ * The mod_doc2interact course module viewed event.
+ *
+ * @package    mod_doc2interact
+ * @copyright  2026 Sergio Alejandro Pilar
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class course_module_viewed extends \core\event\course_module_viewed {
+
+    /**
+     * Init method.
+     */
+    protected function init(): void {
+        $this->data['crud'] = 'r';
+        $this->data['edulevel'] = self::LEVEL_PARTICIPATING;
+        $this->data['objecttable'] = 'doc2interact';
+    }
+}
